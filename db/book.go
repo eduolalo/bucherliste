959284ed14b5b7f4da86bb774f600a9a3276fcfa
Eduardo <rcchristiane@gmail.com>
2022-06-19 11:38:18 -0500

    gorm and basic models added

---
+++ db/book.go
@@ -0,0 +1,23 @@
+package db
+
+import "gorm.io/gorm"
+
+// Book es la estructura para trabajar con los datos de un libro
+type Book struct {
+	gorm.Model
+
+	// Identificador del libro
+	ID []byte `gorm:"type:BINARY(16);not null;unique;default:(UUID_TO_BIN(UUID()))"`
+	// Identificador en google books
+	GID string `gorm:"size:64;not null" validate:"required,max=64" json:"gid"`
+	// Titulo del libro
+	Title string `gorm:"size:64;not null" validate:"required,max=64" json:"name"`
+	// Autor del libro
+	Authors string `gorm:"size:256;not null" validate:"required,max=256" json:"authors"`
+	// Publicador del libro
+	Publisher string `gorm:"size:128;not null" validate:"required,max=128" json:"publisher"`
+
+	/*                                    Relaciones                                    */
+	// Book belongs to a wishlist
+	WishListID []byte `gorm:"type:BINARY(16);not null"`
+}
---
+++ db/user.go
@@ -0,0 +1,20 @@
+package db
+
+import "gorm.io/gorm"
+
+// User es la estructura para trabajar con los datos de un usuario
+type User struct {
+	gorm.Model
+
+	// Identificador público del usuario
+	ID []byte `gorm:"type:BINARY(16);not null;unique;default:(UUID_TO_BIN(UUID()))"`
+	// Nombre(s) del usuario
+	Username string `gorm:"size:64;not null" validate:"required,max=64" json:"username"`
+	// Password
+	Password string `gorm:"-" validate:"required,max=64"  json:"password"`
+	Hash     []byte `gorm:"type:BINARY(32);not null" json:"-"`
+
+	/*  Relaciones  */
+	// Users has many Wishlists
+	WishList []WishList `gorm:"foreignkey:UserID"`
+}
