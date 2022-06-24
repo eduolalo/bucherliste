7572a777a0c5da7f3f69807fae03a7d10c0be128
Eduardo <rcchristiane@gmail.com>
2022-06-23 22:54:14 -0500

    add tests a GH actions

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@
 export API_URL="https://www.googleapis.com/books/v1/volumes?key="
 export API_KEY="AIzaSyAqlSYDVik9vOBuLLhpIK_TNv7bh-VbHrk"
 # Ejecutar linter y después el run tests
- test ./... -v
+go test ./... -v
